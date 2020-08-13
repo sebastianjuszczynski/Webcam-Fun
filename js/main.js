c67400bea9a14fdb4ae7a1325c4a0fab98068641
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-08-13 11:31:14 +0100

    sound added to function takePhoto

--- js/main.js
+++ js/main.js
@@ -26,4 +26,11 @@ function paintToCanvas() {
     }, 16);
 }
 
+function takePhoto() {
+    snap.currentTime = 0;
+    snap.play();
+}
+
 getVideo();
+
+video.addEventListener("canplay", paintToCanvas);
